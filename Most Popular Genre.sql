9b032b42e6603f67e4059cc685a5aafad876d3ac
Tasnim A. <tasu2301@gmail.com>
2024-09-17 07:40:26 +0600

    Update Most Popular Genre.sql

--- Most Popular Genre.sql
+++ Most Popular Genre.sql
@@ -1,7 +1,7 @@
 --Find the Most Popular Genre:
 --Retrieve the GenreId and Name of the genre with the most tracks.
 
-SELECT *, count( DISTINCT CustomerId)
+SELECT g.GenreId , g.Name , count(DISTINCT CustomerId) AS total_customer, COUNT(t.TrackId) AS total_track, SUM(Total) AS total_sales
 FROM genre g
 JOIN Track t 
 ON g.GenreId = t.GenreId
@@ -10,3 +10,5 @@ ON t.TrackId = il.TrackId
 JOIN Invoice i 
 ON il.InvoiceId = i.InvoiceId 
 GROUP BY g.GenreId
+ORDER BY total_customer DESC
+LIMIT 5
