aa79474bd72cffc33b5c29378d085f348b24baa0
Tasnim A. <tasu2301@gmail.com>
2024-09-22 17:25:25 +0600

    non equi joins

--- recomendations.sql
+++ recomendations.sql
@@ -1,13 +1,23 @@
--- list other cabs we can suggests to our customer, based on their price range, seat, and state
+-- list other cabs we can suggests to our customer, 
+-- based on their price range, seat, and state
 
-select c.*
+with AvailableCabs as
+(select c.*
 from cab c
 left join order_details od 
 on c.id  = od.cab_id 
 where c.id not in(select cab_id from order_details)
-order by c.id
+order by c.id)
+select * from AvailableCabs
 
+select *
+from customers c 
+left join AvailableCabs a
+on c.from_state = a.state
+where a.charge between c.min_rent and c.max_rent 
+and c.min_seater >= a.seater
 
 
 select * from cab c ;
 select * from order_details od ;
+select * from customers c ;
